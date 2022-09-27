2aa9a1b55b975ff239bc9b35cacc2c4438423ae7
Nurdaulet123456 <nurdauletdeveloper@gmail.com>
2022-09-27 17:35:05 +0600

    Footer Start

---
+++ src/components/app-footer/App-Footer.js
@@ -0,0 +1,19 @@
+import './App-Footer.scss'
+
+import React from 'react';
+
+const AppFooter = () => {
+  return (
+    <>
+        <footer className="footer">
+            <div className="container">
+                <div className="footer__inner">
+                    asdasd
+                </div>
+            </div>
+        </footer> 
+    </>
+  );
+}
+
+export default AppFooter;
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AppSubmenu from '../app-submenu/App-Submenu';
 import AppAbout from '../app-about/App-About';
+import AppFooter from '../app-footer/App-Footer';
 
 function App() {
   return (
     <>
       <AppSubmenu /> 
       <AppAbout />     
+      <AppFooter />
     </>
   );
 }
