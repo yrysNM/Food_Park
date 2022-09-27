ca8fde322fb6b0e8f45146083212274a95366594
yrysNM <yrys.n3m@gmail.com>
2022-09-27 14:22:35 +0600

    front header kinda done

---
+++ src/components/app-header/AppHeader.js
@@ -0,0 +1,61 @@
+import searchIcon from "../../resources/icons/search.svg";
+import phoneIcon from "../../resources/icons/phone.svg";
+import locationIcon from "../../resources/icons/location.svg";
+
+import "./appHeader.scss";
+
+const AppHeader = () => {
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="nav">
+                    <div className="header__logo">
+                        <h1 className="header__logo-text">Food Park</h1>
+                    </div>
+
+                    <div className="header__info">
+                        <form>
+                            <div className="header__info_form">
+                                <img src={locationIcon} alt="location icon" className="header__info_locationIcon" />
+                                <input
+                                    type="text"
+                                    className="header__info_search"
+                                    placeholder="Введите адрес доставки" />
+                                <img src={searchIcon} alt="search icon" className="header__info_searchIcon" />
+                            </div>
+                        </form>
+
+                        <div className="header__info_contact">
+                            <div className="header__info_phone">
+                                <img src={phoneIcon} alt="phone icon" className="header__info_phoneIcon" />
+                            </div>
+                            <div className="header__info_phoneText">
+                                <div className="headTitle">
+                                    Контакты:
+                                    <br />
+                                    <a href="tel:+79175105759" className="phoneText">
+                                        +7 (917) 510-57-59
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="header__basket">
+                        <div className="header__basket_wrapper">
+                            <div className="header__basket_text">
+                                Корзина
+                            </div>
+                            <div className="header__basket_count">
+                                <div className="countText">4</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+    );
+}
+
+export default AppHeader;
