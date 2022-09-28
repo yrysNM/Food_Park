8939649282d96bbe1dedef35b47a817af1949ce1
Nurdaulet123456 <nurdauletdeveloper@gmail.com>
2022-09-28 22:03:06 +0600

    Footer finish and scrollTo still

--- src/components/app-footer/App-Footer.js
+++ src/components/app-footer/App-Footer.js
@@ -1,19 +1,68 @@
-import './App-Footer.scss'
+import "./App-Footer.scss";
+import Arrow from "../../resurce/img/icons/arrow.svg";
 
-import React from 'react';
+import React, { useEffect } from "react";
 
 const AppFooter = () => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, []);
+
   return (
     <>
-        <footer className="footer">
-            <div className="container">
-                <div className="footer__inner">
-                    asdasd
-                </div>
+      <footer className="footer">
+        <div className="container">
+          <div className="footer__inner">
+            <div
+              className="arrow__top"
+              onClick={() => {
+                window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+              }}
+            >
+              <img src={Arrow} alt="" />
             </div>
-        </footer> 
+
+            <div className="footer__content">
+              <div className="footer__logo">LOGOS</div>
+
+              <div className="footer__content-links">
+                <div>
+                  <a href="/" className="content__link">
+                    Пользовательское соглашение
+                  </a>
+                </div>
+                <div>
+                  <a href="/" className="content__link">
+                    Карта сайта
+                  </a>
+                </div>
+                <div>
+                  <a href="/" className="content__link">
+                    Политика конфиденциальности
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="footer__links">
+              <a href="/" className="footer__link-nav">
+                О ресторане
+              </a>
+              <a href="/" className="footer__link-nav">
+                Условия доставки
+              </a>
+              <a href="/" className="footer__link-nav">
+                Возврат товара
+              </a>
+              <a href="/" className="footer__link-nav">
+                Акции
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
-}
+};
 
 export default AppFooter;
